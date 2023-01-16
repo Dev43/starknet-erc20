6edfa1417d45a464e9eb028e831177f9f0f617d7
Dev43 <patrick@bitbackapp.com>
2023-01-16 16:02:22 -0500

    adds ex15

---
+++ contracts/solutions/ex15/cmd.sh
@@ -0,0 +1,12 @@
+echo "In" $(pwd)
+echo
+if [ "$1" == "deploy" ]; then 
+    echo "deploying..."
+    starknet deploy  --network alpha-goerli --class_hash $2
+elif [ "$1" == "declare" ]; then 
+    echo "declaring..."
+    starknet declare --contract  ./ExcerciseSolution_compiled.json --network alpha-goerli 
+else 
+    echo "compiling..."
+    starknet-compile ./ExcerciseSolution.cairo --output ./ExcerciseSolution_compiled.json
+fi
